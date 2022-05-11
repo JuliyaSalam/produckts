6dad13e98074e90f1475e879d45466eee91aff5b
Jul <yuosalamatova@gmail.com>
2022-05-11 16:10:49 +0700

    +exception

--- src/main/java/ru/netology/Repository.java
+++ src/main/java/ru/netology/Repository.java
@@ -3,15 +3,17 @@ package ru.netology;
 public class Repository {
     private Product[] products = new Product[0];
 
-
-
     public void save(Product product) {
+        if (findById(product.getId()) != null) {
+            throw new AlreadyExistsException("Элеммента с id = " + product.getId() + " уже есть");
+        } else {
 
-        int length = products.length + 1;
-        Product[] tmp = new Product[length];
-        System.arraycopy(products, 0, tmp, 0, products.length);
-        tmp[tmp.length - 1] = product;
-        this.products = tmp;
+            int length = products.length + 1;
+            Product[] tmp = new Product[length];
+            System.arraycopy(products, 0, tmp, 0, products.length);
+            tmp[tmp.length - 1] = product;
+            this.products = tmp;
+        }
     }
 
     public Product[] findAll() {
@@ -19,16 +21,29 @@ public class Repository {
     }
 
     public void dalitId(int id) {
-        int length = products.length -1;
-        Product[] tmp = new Product[length];
-        int index = 0;
-        for (Product item : products) {
-            if (item.getId() != id) {
-                tmp[index] = item;
-                index++;
+        if (findById(id) == null) {
+            throw new NotFoundException("Элеммента с id = " + id + " нет");
+        } else {
+            int length = products.length - 1;
+            Product[] tmp = new Product[length];
+            int index = 0;
+            for (Product item : products) {
+                if (item.getId() != id) {
+                    tmp[index] = item;
+                    index++;
+                }
+            }
+            products = tmp;
+        }
+    }
+
+    public Product findById(int id) {
+        for (Product product : products) {
+            if (product.getId() == id) {
+                return product;
             }
         }
-        products = tmp;
+        return null;
     }
 
 }
--- src/test/java/ru/netology/ManagerTest.java
+++ src/test/java/ru/netology/ManagerTest.java
@@ -30,9 +30,27 @@ class ManagerTest {
         Product[] actual = manager.findAll();
         Product[] expected = {bookAAA, bookBBB, bookCCC, bookAAB, smartphone001, smartphone002, smartphone012, smartphone022};
         assertArrayEquals(expected, actual);
-
-
     }
+
+    @Test
+    void addTestNull() {
+        Book bookAAA = new Book(1, "AAA", 1000, "ABT1");
+        Book bookBBB = new Book(3, "BBB", 500, "ABT1");
+        Book bookCCC = new Book(11, "CCC", 1000, "ABT2");
+        Book bookAAB = new Book(13, "AAA", 10000, "ABT3");
+
+        Manager manager = new Manager();
+
+        manager.add(bookAAA);
+        manager.add(bookBBB);
+        manager.add(bookCCC);
+        manager.add(bookAAB);
+
+        assertThrows(AlreadyExistsException.class, () -> {
+            manager.add(bookAAA);
+        });
+    }
+
 
     @Test
     void searchByTest() {
@@ -86,5 +104,70 @@ class ManagerTest {
         Product[] actual = manager.findAll();
         Product[] expected = {bookAAA, bookBBB, bookCCC, bookAAB, smartphone002, smartphone012, smartphone022};
         assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void dallTestNull() {
+        Book bookAAA = new Book(1, "AAA", 1000, "ABT1");
+        Book bookBBB = new Book(3, "BBB", 500, "ABT1");
+        Book bookCCC = new Book(11, "CCC", 1000, "ABT2");
+        Book bookAAB = new Book(13, "AAA", 10000, "ABT3");
+        Smartphone smartphone001 = new Smartphone(2, "A01", 5000, "A1");
+        Smartphone smartphone002 = new Smartphone(4, "A02", 6000, "A2");
+        Smartphone smartphone012 = new Smartphone(6, "A02", 5000, "A1");
+        Smartphone smartphone022 = new Smartphone(8, "A03", 11000, "A5");
+        Manager manager = new Manager();
+
+        manager.add(bookAAA);
+        manager.add(bookBBB);
+        manager.add(bookCCC);
+        manager.add(bookAAB);
+        manager.add(smartphone001);
+        manager.add(smartphone002);
+        manager.add(smartphone012);
+        manager.add(smartphone022);
+
+        assertThrows(NotFoundException.class, () -> {
+            manager.dall(20);
+        });
+    }
+
+    @Test
+    void dallTestNullRepo() {
+
+        Book bookAAA = new Book(1, "AAA", 1000, "ABT1");
+        Book bookBBB = new Book(3, "BBB", 500, "ABT1");
+        Book bookCCC = new Book(11, "CCC", 1000, "ABT2");
+        Book bookAAB = new Book(13, "AAA", 10000, "ABT3");
+
+        Repository repo = new Repository();
+
+        repo.save(bookAAA);
+        repo.save(bookBBB);
+        repo.save(bookCCC);
+        repo.save(bookAAB);
+
+        assertThrows(NotFoundException.class, () -> {
+            repo.dalitId(2);
+        });
+    }
+
+    @Test
+    void addTestNullRepo() {
+        Book bookAAA = new Book(1, "AAA", 1000, "ABT1");
+        Book bookBBB = new Book(3, "BBB", 500, "ABT1");
+        Book bookCCC = new Book(11, "CCC", 1000, "ABT2");
+        Book bookAAB = new Book(13, "AAA", 10000, "ABT3");
+
+        Repository repo = new Repository();
+
+        repo.save(bookAAA);
+        repo.save(bookBBB);
+        repo.save(bookCCC);
+        repo.save(bookAAB);
+
+        assertThrows(AlreadyExistsException.class, () -> {
+            repo.save(bookAAA);
+        });
     }
 }
